50a161c2be61b21a6e8362706fb5e7559a1952fd
blazingh <hadibaalbaki2@gmail.com>
2022-05-26 18:59:16 +0300

    batch 1

---
+++ mysite/main/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.utils import timezone
+
+class exercise(models.Model):
+    name = models.CharField(max_length=30)
+    equip = models.CharField(max_length=15)
+    muscles = models.CharField(max_length=15)
+    risk = models.CharField(max_length=15)
+    motion = models.CharField(max_length=10)
+    level = models.IntegerField()
+    time = models.IntegerField()
+    space = models.IntegerField()
+    details = models.TextField()
+
+    def __str__(self):
+        return self.name
---
+++ mysite/main/static/main/muscles.js
@@ -0,0 +1,142 @@
+function pick(svg, check) {
+    const grp = document.getElementById(check)
+    const id = "#" + svg + " path"
+
+    if (grp.checked) {
+        for (const e of document.querySelectorAll(id)) {
+            e.setAttribute("style", "fill: grey;");
+        }
+        grp.setAttribute("checked", "false")
+        grp.checked = false;
+    } else if (!grp.checked) {
+        for (const e of document.querySelectorAll(id)) {
+            e.setAttribute("style", "fill: rgb(255, 77, 77) ;");
+        }
+        grp.setAttribute("checked", "true")
+        grp.checked = true;
+    }
+}
+
+
+function pick2(svg, check) {
+    const grp = document.getElementById(check)
+    const id = "#" + svg + " path"
+
+    if (!grp.checked) {
+        for (const e of document.querySelectorAll(id)) {
+            e.setAttribute("style", "fill: grey;");
+        }
+    } else if (grp.checked) {
+        for (const e of document.querySelectorAll(id)) {
+            e.setAttribute("style", "fill: rgb(255, 77, 77) ;");
+        }
+    }
+}
+
+document.querySelectorAll('.groups').forEach(item => {
+    item.addEventListener('click', event => {
+        pick(item.id, item.getAttribute("name"))
+    })
+})
+
+document.querySelectorAll('.checks').forEach(item => {
+    item.addEventListener('click', event => {
+        pick2(item.getAttribute("value"), item.id)
+    })
+})
+
+var slider = document.getElementById("time")
+var tim = document.getElementById("tim")
+
+tim.innerHTML = slider.value;
+
+slider.oninput = function() {
+    document.getElementById("tim").innerHTML = this.value;
+}
+
+for (const e of document.querySelectorAll(".equip")) {
+    e.addEventListener("click", event => {
+        document.getElementById("eno").setAttribute("checked", "false");
+        document.getElementById("eno").checked = false;
+    })
+}
+
+document.getElementById("noe").addEventListener("click", event => {
+    for (const e of document.querySelectorAll(".equip")) {
+        e.setAttribute("checked", "false");
+        e.checked = false;
+    }
+    document.getElementById("eno").checked = false
+    document.getElementById("eno").setAttribute("checked","false")
+})
+
+for (const e of document.querySelectorAll(".risks")) {
+    e.addEventListener("click", event => {
+        document.getElementById("rno").setAttribute("checked", "false");
+        document.getElementById("rno").checked = false;
+    })
+}
+
+document.getElementById("nor").addEventListener("click", event => {
+    for (const e of document.querySelectorAll(".risks")) {
+        e.setAttribute("checked", "false");
+        e.checked = false;
+    }
+    document.getElementById("rno").checked = false
+    document.getElementById("rno").setAttribute("checked","false") 
+
+})
+
+document.addEventListener('click', function(event) {
+    for (const e of document.querySelectorAll('.infocheck:checked ~ .hoverinfo')) {
+        if (e !== event.target && !e.contains(event.target)) {
+            for (const i of document.querySelectorAll(".infocheck")) {
+                    if (i !== event.target && !i.contains(event.target)) {
+                        i.checked = false
+                        i.setAttribute("checked", "fasle")
+                        console.log("outside click")
+                    }
+                }
+            
+        }
+    }
+})
+
+function quickselect(part){
+    if (part=="full"){
+        for (const m of document.querySelectorAll(".checks")){
+            m.checked = true
+            m.setAttribute("checked", "true")
+            pick2(m.getAttribute("value"), m.id)
+        }
+        return
+    }
+    if (part=="upper"){
+        var section = ["tra", "del", "bic", "tri", "for", "che", "lat", "nec"]
+        for (const m of document.querySelectorAll(".checks")){
+            m.checked = false
+            m.setAttribute("checked", "false")
+            pick2(m.getAttribute("value"), m.id)
+            if (section.includes(m.id)){
+                m.checked = true
+                m.setAttribute("checked", "true")
+                pick2(m.getAttribute("value"), m.id)
+            }
+        }
+        return
+    }
+    if (part=="lower"){
+        var section = ["qua", "glu", "ham", "cal", "shi"]
+        for (const m of document.querySelectorAll(".checks")){
+            m.checked = false
+            m.setAttribute("checked", "false")
+            pick2(m.getAttribute("value"), m.id)
+            if (section.includes(m.id)){
+                m.checked = true
+                m.setAttribute("checked", "true")
+                pick2(m.getAttribute("value"), m.id)
+            }
+        }
+        return
+    }
+}
